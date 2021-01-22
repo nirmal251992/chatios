ee594b5c8f83cb3f8489bf03eca417317795aa0a
nirmal <nirmal251992@gmail.com>
2021-01-22 13:46:01 +0530

    Initial Commit

---
+++ chatview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatview
+//
+//  Created by Nirmal on 22/01/21.
+//  Copyright © 2021 Nirmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
